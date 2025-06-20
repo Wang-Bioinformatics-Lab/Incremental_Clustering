ff925a94f1719c4325d84092d0060b5789f5de3e
XianghuWang-287 <wangxianghu1019@126.com>
2025-06-20 12:54:44 -0700

    fixed the bugs and pin the version

--- bin/summarize_results.py
+++ bin/summarize_results.py
@@ -22,27 +22,27 @@ def main():
 
     # Grouping by cluster
     grouped_cluster_df = clusterinfo_df.groupby(["cluster"])
-    cluster_summary_list = []
-    for cluster, cluster_group_df in grouped_cluster_df:
-        #TODO :Read these from mgf, as the representative is a medoid
-
-        cluster_count = len(cluster_group_df)
-        cluster_mz = cluster_group_df["precursor_mz"].mean()
-        cluster_rt = cluster_group_df["retention_time"].mean()
-        cluster_charge = cluster_group_df["precursor_charge"].mean()
-
-        output_dict = {}
-        output_dict["count"] = cluster_count
-        output_dict["precursor_mz"] = cluster_mz
-        output_dict["retention_time"] = cluster_rt
-        output_dict["charge"] = cluster_charge
-        output_dict["scan"] = cluster[0] + 1
-
-        cluster_summary_list.append(output_dict)
-
-    # Creating a cluster summary
-    cluster_summary_df = pd.DataFrame(cluster_summary_list)
-    cluster_summary_df.to_csv(os.path.join(args.output_summary_folder, "cluster_summary.tsv"), sep='\t', index=False)
+    # cluster_summary_list = []
+    # for cluster, cluster_group_df in grouped_cluster_df:
+    #     #TODO :Read these from mgf, as the representative is a medoid
+    #
+    #     cluster_count = len(cluster_group_df)
+    #     cluster_mz = cluster_group_df["precursor_mz"].mean()
+    #     cluster_rt = cluster_group_df["retention_time"].mean()
+    #     cluster_charge = cluster_group_df["precursor_charge"].mean()
+    #
+    #     output_dict = {}
+    #     output_dict["count"] = cluster_count
+    #     output_dict["precursor_mz"] = cluster_mz
+    #     output_dict["retention_time"] = cluster_rt
+    #     output_dict["charge"] = cluster_charge
+    #     output_dict["scan"] = cluster[0] + 1
+    #
+    #     cluster_summary_list.append(output_dict)
+    #
+    # # Creating a cluster summary
+    # cluster_summary_df = pd.DataFrame(cluster_summary_list)
+    # cluster_summary_df.to_csv(os.path.join(args.output_summary_folder, "cluster_summary.tsv"), sep='\t', index=False)
 
     # Creating cluster info
     clusterinfo_df["filename"] = clusterinfo_df["identifier"].apply(lambda x: x.split(":")[2] + ".mzML")
